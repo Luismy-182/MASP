851769ba1bddfbb0259ec55cd50c4c9d614b7e33
Miguel Suarez <miguel.buny@gmail.com>
2026-03-17 11:10:36 -0600

    trabajando en los repos

--- src/components/ProjectSlider.jsx
+++ src/components/ProjectSlider.jsx
@@ -2,6 +2,8 @@
 import { Swiper, SwiperSlide } from "swiper/react"
 import { Navigation, Pagination } from "swiper/modules"
 import { useRef } from "react"
+import { useState } from "react"
+
 
 import "swiper/css"
 import "swiper/css/navigation"
@@ -11,22 +13,23 @@ import { ChevronLeft, ChevronRight } from "lucide-react"
 
 const ProjectSlider = ({ images = [] }) => {
 
+  const [current, setCurrent] = useState(1); //diapositivas
   const prevRef = useRef(null)
   const nextRef = useRef(null)
 
   return (
-    <div className="relative h-full">
+    <div className="relative h-full group">
 
       <button
         ref={prevRef}
-        className="absolute left-2 top-1/2 -translate-y-1/2 z-10 flex items-center justify-center"
+        className="absolute left-3 top-1/2 -translate-y-1/2 z-10 flex items-center justify-center opacity-0 group-hover:opacity-100 transition hover:scale-110 hover:cursor-pointer "
       >
-        <ChevronLeft className="w-8 h-8 rounded-full bg-white/80 hover:bg-white shadow" />
+        <ChevronLeft className="w-8 h-8 rounded-full bg-white/80 hover:bg-white shadow " />
       </button>
 
       <button
         ref={nextRef}
-        className="absolute right-2 top-1/2 -translate-y-1/2 z-10 flex items-center justify-center"
+        className="absolute right-3 top-1/2 -translate-y-1/2 z-10 flex items-center justify-center opacity-0 group-hover:opacity-100 transition hover:scale-110 hover:cursor-pointer "
       >
         <ChevronRight className="w-8 h-8 rounded-full bg-white/80 hover:bg-white shadow" />
       </button>
@@ -34,27 +37,36 @@ const ProjectSlider = ({ images = [] }) => {
       <Swiper
         modules={[Navigation, Pagination]}
         slidesPerView={1}
-        pagination={{ clickable: true }}
+
         onBeforeInit={(swiper) => {
           swiper.params.navigation.prevEl = prevRef.current
           swiper.params.navigation.nextEl = nextRef.current
         }}
         navigation
         className="h-full"
+        onSlideChange={(swiper) => setCurrent(swiper.activeIndex + 1)}
       >
+
         {images.map((img, index) => (
           <SwiperSlide key={index}>
             <div className="w-full h-full">
               <img
                 loading="lazy"
                 src={img}
-                className="object-fill w-full h-full"
+                className="object-contain w-full h-full bg-neutral-900  p-2 cursor-grab active:cursor-grabbing"
                 alt="imagen-portafolio"
               />
+
+
             </div>
           </SwiperSlide>
         ))}
       </Swiper>
+
+      <div className="absolute bottom-3 right-3 z-10 text-xs bg-black/60 text-white px-2 py-1 rounded-md backdrop-blur-sm">
+        {current} / {images.length}
+      </div>
+
 
     </div>
   )
--- src/helpers/projectData.js
+++ src/helpers/projectData.js
@@ -7,7 +7,8 @@ const freshCoffee = {
     title: 'Fresh Coffee — Sistema de pedidos para quiosco de comida',
     description: 'Aplicación full-stack para la gestión de pedidos en un quiosco de comida.Los usuarios pueden registrarse, explorar un catálogo de bebidas y platillos, crear pedidos y calcular automáticamente el total de la orden. Incluye autenticación de usuarios, protección de rutas y un panel administrativo para gestionar productos y visualizar pedidos en tiempo real.',
     tags: ['Laravel' , 'React' , 'REST API' , 'Context API' , 'SWR' , 'TailwindCSS'],
-    images: [`${rutaFreshCoffe}1-login.png`, `${rutaFreshCoffe}2-dashboard.png`, `${rutaFreshCoffe}3-pedido.png`, `${rutaFreshCoffe}4-pedidos.png`, `${rutaFreshCoffe}5-agotar.png`]
+    images: [`${rutaFreshCoffe}1-login.png`, `${rutaFreshCoffe}2-dashboard.png`, `${rutaFreshCoffe}3-pedido.png`, `${rutaFreshCoffe}4-pedidos.png`, `${rutaFreshCoffe}5-agotar.png`],
+    repo:[{frontend:'https://github.com/Luismy-182/React-FreshCoffe', backend:'https://github.com/Luismy-182/Laravel-FreshCoffe.git'}]
 }
 
 const apv = {
@@ -15,14 +16,16 @@ const apv = {
     title: 'APV — Sistema de gestión de pacientes veterinarios',
     description: 'Aplicación web desarrollada con el stack MERN para la gestión de pacientes en clínicas veterinarias.Permite a los veterinarios registrarse, autenticarse y administrar el registro de sus propios pacientes mediante un panel privado. El sistema incluye recuperación de contraseña por correo electrónico, validaciones en tiempo real y control de acceso para garantizar que cada usuario gestione únicamente su propia información.',
     tags: ['MongoDB', 'Express', 'React','Nodejs','JWT', 'Context API','Mongoose' ,'TailwindCSS'],
-    images: [`${rutaAPV}1-login.png`, `${rutaAPV}2-form.png`, `${rutaAPV}3-created.png`, `${rutaAPV}4-edit.png`]
+    images: [`${rutaAPV}1-login.png`, `${rutaAPV}2-form.png`, `${rutaAPV}3-created.png`, `${rutaAPV}4-edit.png`],
+    repo:[{git:'https://github.com/Luismy-182/APV-MERN---Full-Stack'}]
 }
 const devJobs = {
     id: 'devJobs',
     title: 'DevJobs — Plataforma de publicación y gestión de empleos',
     description: 'Aplicación web desarrollada con Laravel que permite publicar y gestionar vacantes laborales desde una plataforma similar a un portal de empleo. Incluye autenticación de usuarios, gestión de roles y panel administrativo para administrar ofertas de trabajo.',
     tags: ['Laravel' , 'Livewire' , 'TailwindCSS' , 'Laravel Breeze' , 'Eloquent ORM'],
-    images: [`${rutaDevjobs}1-dashboard.png`, `${rutaDevjobs}2-vacantes.png`, `${rutaDevjobs}3-vacante.png`, `${rutaDevjobs}4-admin.png`, `${rutaDevjobs}5-publicar.png`]
+    images: [`${rutaDevjobs}1-dashboard.png`, `${rutaDevjobs}2-vacantes.png`, `${rutaDevjobs}3-vacante.png`, `${rutaDevjobs}4-admin.png`, `${rutaDevjobs}5-publicar.png`],
+    repo:[{git:'https://github.com/Luismy-182/devjobs.git'}]
 }
 
 export {
